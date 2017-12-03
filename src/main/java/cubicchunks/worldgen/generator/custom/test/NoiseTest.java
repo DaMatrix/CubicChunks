bb0e2eccd94330a3dda43c02f1016413f4d7e923
DaMatrix <joey.rabil@gmail.com>
2017-12-03 15:37:57 +0100

    push bebause barteks wanted to see code

--- src/main/java/cubicchunks/worldgen/generator/custom/test/NoiseTest.java
+++ src/main/java/cubicchunks/worldgen/generator/custom/test/NoiseTest.java
@@ -1,28 +1,29 @@
 package cubicchunks.worldgen.generator.custom.test;
 
-import com.flowpowered.noise.NoiseQuality;
-import com.flowpowered.noise.module.source.Perlin;
+import cubicchunks.worldgen.generator.custom.builder.IBuilder;
+import cubicchunks.worldgen.generator.custom.builder.NoiseSource;
 
 public class NoiseTest {
     public static void main(String... args) {
-        Perlin groundNoise = new Perlin();
-        groundNoise.setNoiseQuality(NoiseQuality.STANDARD);
-        groundNoise.setOctaveCount(4);
-        groundNoise.setFrequency(0.008);
-        groundNoise.setSeed((int) (System.currentTimeMillis() / (Math.pow(2, 32))));
+        IBuilder builder = NoiseSource.perlin()
+                .frequency(0.06)
+                .octaves(6)
+                .normalizeTo(-1, 1)
+                .seed(System.currentTimeMillis())
+                .create();
 
         double min = Double.MAX_VALUE, max = Double.MIN_VALUE;
-        for (int x = 0; x < 900000; x++)    {
-            double value = groundNoise.getValue(x, 0,0);
-            //value *= (400 - 3) / 2;
-            //value += (400 + 3) / 2;
-            value *= 256;
-            if (value > max)    {
-                max = value;
-            } else if (value < min) {
+        for (int x = -100000; x < 900000; x++)    {
+            double valueX = builder.get(x, 0, 0) * 256;
+            double valueY = builder.get(0, x, 0) * 256;
+            double valueZ = builder.get(0, 0, x) * 256;
+            double value = (valueX + valueY + valueZ) / 3;
+            if (value < min)    {
                 min = value;
+            } else if (value > max) {
+                max = value;
             }
-            System.out.println(value);
+            System.out.println((value > 0) + " " + (valueX > 0) + " " + (valueY > 0) + " " + (valueZ > 0));
         }
         System.out.println(min + " " + max);
     }
