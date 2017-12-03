35f15ddf2bbab742002fcbff441cc54f9ab46da6
DaMatrix <joey.rabil@gmail.com>
2017-12-03 11:37:11 +0100

    make terrain decorate correctly

--- src/main/java/cubicchunks/worldgen/generator/custom/CustomTerrainGenerator.java
+++ src/main/java/cubicchunks/worldgen/generator/custom/CustomTerrainGenerator.java
@@ -42,6 +42,7 @@ import cubicchunks.worldgen.generator.CubePrimer;
 import cubicchunks.worldgen.generator.ICubePrimer;
 import cubicchunks.worldgen.generator.custom.biome.replacer.IBiomeBlockReplacer;
 import cubicchunks.worldgen.generator.custom.builder.BiomeSource;
+import cubicchunks.worldgen.generator.custom.builder.NoiseConsumer;
 import cubicchunks.worldgen.generator.custom.structure.CubicCaveGenerator;
 import cubicchunks.worldgen.generator.custom.structure.CubicRavineGenerator;
 import cubicchunks.worldgen.generator.custom.structure.CubicStructureGenerator;
@@ -53,6 +54,8 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.monster.EntityShulker;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
@@ -65,7 +68,6 @@ import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -79,24 +81,15 @@ public class CustomTerrainGenerator extends BasicCubeGenerator {
     private final CustomGeneratorSettings conf;
     private final ChunkGeneratorEnd endChunkGenerator;
     private Perlin groundNoise;
-    private LoadingCache<Long, Integer[][]> groundNoiseCache = CacheBuilder.<Long, Integer[][]>newBuilder()
+    private LoadingCache<Long, Integer> groundNoiseCache = CacheBuilder.<Long, Integer>newBuilder()
             .expireAfterAccess(1, TimeUnit.MINUTES)
             .maximumSize(512)
-            .build(new CacheLoader<Long, Integer[][]>() {
+            .build(new CacheLoader<Long, Integer>() {
                 @Override
-                public Integer[][] load(Long key) {
+                public Integer load(Long key) {
                     int x = (int) (key >> 32);
                     int y = (int) (long) key;
-                    x *= 16;
-                    y *= 16;
-                    Integer[][] heights = new Integer[16][16];
-                    for (int genX = 0; genX < heights.length; genX++) {
-                        Integer[] a = heights[genX];
-                        for (int genY = 0; genY < a.length; genY++) {
-                            a[genY] = (int) (groundNoise.getValue(x + genX, y + genY, 0) * 256);
-                        }
-                    }
-                    return heights;
+                    return (int) (groundNoise.getValue(x, y, 0) * 256);
                 }
             });
     private Chunk endChunkCache = null;
@@ -137,73 +130,6 @@ public class CustomTerrainGenerator extends BasicCubeGenerator {
         generate(primer, cubeX, cubeY, cubeZ);
         generateStructures(primer, new CubePos(cubeX, cubeY, cubeZ));
         return primer;
-    }
-
-    @Override
-    public void populate(Cube cube) {
-        if (PorkMethods.isCubeOutOfBounds(cube.getCoords())) {
-            return;
-        }
-
-        if (!MinecraftForge.EVENT_BUS.post(new CubePopulatorEvent(world, cube))) {
-            CubicBiome biome = CubicBiome.getCubic(cube.getCubicWorld().getBiome(Coords.getCubeCenter(cube)));
-
-            CubePos pos = cube.getCoords();
-            // For surface generators we should actually use special RNG with
-            // seed
-            // that depends only in world seed and cube X/Z
-            // but using this for surface generation doesn't cause any
-            // noticeable issues
-            Random rand = new Random(cube.cubeRandomSeed());
-
-            if (cube.getY() >= 1000 && cube.getY() <= 1015) {
-                endChunkGenerator.populate(cube.getX(), cube.getZ());
-
-                double j = rand.nextDouble();
-                if (j > 0.9) {
-                    BlockPos blockPos = new BlockPos(rand.nextInt(16) + cube.getCoords().getXCenter(), 16080, rand.nextInt(16) + cube.getCoords().getZCenter());
-                    for (; blockPos.y > 16030; blockPos.y--) {
-                        if (world.getBlockState(blockPos).getBlock() == Blocks.END_STONE) {
-                            break;
-                        }
-                    }
-
-                    if (blockPos.getY() > 16032) {
-                        blockPos.y += 1;
-                        BlockChorusFlower.generatePlant(World.class.cast(world), blockPos, rand, 8);
-                    }
-                }
-
-                j = rand.nextDouble();
-                if (j > 0.99) {
-                    BlockPos blockPos = new BlockPos(rand.nextInt(16) + cube.getCoords().getXCenter(), 16080, rand.nextInt(16) + cube.getCoords().getZCenter());
-                    for (; blockPos.y > 16030; blockPos.y--) {
-                        if (world.getBlockState(blockPos).getBlock() == Blocks.END_STONE) {
-                            break;
-                        }
-                    }
-
-                    if (blockPos.getY() > 16032) {
-                        blockPos.y += 1;
-                        EntityShulker shulker = new EntityShulker(World.class.cast(world));
-                        world.spawnEntity(shulker);
-                        shulker.setPosition(blockPos.x + .5f, blockPos.y, blockPos.z + .5f);
-                    }
-                }
-
-                return;
-            }
-
-            ICubicPopulator decorator = biome.getDecorator();
-            decorator.generate(world, rand, pos, biome);
-            CubeGeneratorsRegistry.generateWorld(world, rand, pos, biome);
-
-            strongholds.generateStructure((World) world, rand, pos);
-        }
-    }
-
-    private IBlockState chooseState(Random random, IBlockState... states) {
-        return states[random.nextInt(states.length)];
     }
 
     @Override
@@ -289,49 +215,28 @@ public class CustomTerrainGenerator extends BasicCubeGenerator {
             return;
         }
 
-        try {
-            Integer[][] groundNoiseArr = groundNoiseCache.get((((long) cubeX) << 32) | (cubeZ & 0xffffffffL));
-            int cubeAbsoluteX = cubeX * 16;
-            int cubeAbsoluteY = cubeY * 16;
-            int cubeAbsoluteZ = cubeZ * 16;
-            IBlockState air = Blocks.AIR.getDefaultState();
-            for (int x = 0, absX = cubeAbsoluteX; x < Cube.SIZE; x++, absX++) {
-                Integer[] groundNoiseAtX = groundNoiseArr[x];
-                for (int z = 0, absZ = cubeAbsoluteZ; z < Cube.SIZE; z++, absZ++) {
-                    int groundNoise = groundNoiseAtX[z];
-                    for (int y = 0, absY = cubeAbsoluteY; absY < groundNoise; y++, absY++) {
-                        List<IBiomeBlockReplacer> replacers = biomeSource.getReplacers(absX, absY, absZ);
-                        IBlockState block = Blocks.AIR.getDefaultState();
-                        int size = replacers.size();
-                        double density = groundNoise - absY;
-                        for (int i = 0; i < size; i++) {
-                            block = replacers.get(i).getReplacedBlock(block, absX, absY, absZ, /*dx, dy, dz, density*/1, 1, 1, density);
-                        }
+        BlockPos start = new BlockPos(cubeX * 4, cubeY * 2, cubeZ * 4);
+        BlockPos end = start.add(4, 2, 4);
+        IBlockState air = Blocks.AIR.getDefaultState();
+        this.forEachScaled(start, end, new Vec3i(4, 8, 4),
+                (x, y, z, dx, dy, dz, v) -> {
+                    List<IBiomeBlockReplacer> replacers = biomeSource.getReplacers(x, y, z);
+                    IBlockState block = Blocks.AIR.getDefaultState();
+                    int size = replacers.size();
+                    for (int i = 0; i < size; i++) {
+                        block = replacers.get(i).getReplacedBlock(block, x, y, z, dx, dy, dz, v);
+                    }
 
-                        if (block != air) {
-                            cubePrimer.setBlockState(x, y, z, block);
-                        }
+                    if (block != air) {
+                        cubePrimer.setBlockState(x & 0xf, y & 0xf, z & 0xf, block);
                     }
                 }
-            }
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+        );
     }
 
-    /**
-     * Retrieve the blockstate appropriate for the specified builder entry
-     *
-     * @return The block state
-     */
-    private IBlockState getBlock(int x, int y, int z, double dx, double dy, double dz, double density) {
-        List<IBiomeBlockReplacer> replacers = biomeSource.getReplacers(x, y, z);
-        IBlockState block = Blocks.AIR.getDefaultState();
-        int size = replacers.size();
-        for (int i = 0; i < size; i++) {
-            block = replacers.get(i).getReplacedBlock(block, x, y, z, dx, dy, dz, density);
-        }
-        return block;
+    public double get(int x, int y, int z) {
+        double groundNoise = groundNoiseCache.getUnchecked((((long) x) << 32) | (z & 0xffffffffL));
+        return MathHelper.clamp((groundNoise - y) / 255, -1, 1);
     }
 
     private void generateStructures(ICubePrimer cube, CubePos cubePos) {
@@ -346,5 +251,214 @@ public class CustomTerrainGenerator extends BasicCubeGenerator {
             this.strongholds.generate(world, cube, cubePos);
         }
 
+    }
+
+    /**
+     * had to copypasta this from IBuilder to avoid using IBuilder because i don't want to use it, but this method is important
+     */
+    public void forEachScaled(Vec3i startUnscaled, Vec3i endUnscaled, Vec3i scale, NoiseConsumer consumer) {
+
+        if (scale.getZ() != scale.getX()) {
+            throw new UnsupportedOperationException("X and Z scale must be the same!");
+        }
+        final double/*[]*/[][] gradX = new double/*[scale.getX()]*/[scale.getY()][scale.getZ()];
+        final double[]/*[]*/[] gradY = new double[scale.getX()]/*[scale.getY()]*/[scale.getZ()];
+        final double[][]/*[]*/ gradZ = new double[scale.getX()][scale.getY()]/*[scale.getZ()]*/;
+        final double[][][] vals = new double[scale.getX()][scale.getY()][scale.getZ()];
+
+        int xScale = scale.getX();
+        int yScale = scale.getY();
+        int zScale = scale.getZ();
+
+        double stepX = 1.0 / xScale;
+        double stepY = 1.0 / yScale;
+        double stepZ = 1.0 / zScale;
+
+        int minX = startUnscaled.getX();
+        int minY = startUnscaled.getY();
+        int minZ = startUnscaled.getZ();
+        int maxX = endUnscaled.getX();
+        int maxY = endUnscaled.getY();
+        int maxZ = endUnscaled.getZ();
+        for (int sectionX = minX; sectionX < maxX; ++sectionX) {
+            int x = sectionX * xScale;
+            for (int sectionZ = minZ; sectionZ < maxZ; ++sectionZ) {
+                int z = sectionZ * zScale;
+                for (int sectionY = minY; sectionY < maxY; ++sectionY) {
+                    int y = sectionY * yScale;
+
+                    final double v000 = this.get(x + xScale * 0, y + yScale * 0, z + zScale * 0);
+                    final double v001 = this.get(x + xScale * 0, y + yScale * 0, z + zScale * 1);
+                    final double v010 = this.get(x + xScale * 0, y + yScale * 1, z + zScale * 0);
+                    final double v011 = this.get(x + xScale * 0, y + yScale * 1, z + zScale * 1);
+                    final double v100 = this.get(x + xScale * 1, y + yScale * 0, z + zScale * 0);
+                    final double v101 = this.get(x + xScale * 1, y + yScale * 0, z + zScale * 1);
+                    final double v110 = this.get(x + xScale * 1, y + yScale * 1, z + zScale * 0);
+                    final double v111 = this.get(x + xScale * 1, y + yScale * 1, z + zScale * 1);
+
+                    double v0y0 = v000;
+                    double v0y1 = v001;
+                    double v1y0 = v100;
+                    double v1y1 = v101;
+                    final double d_dy__0y0 = (v010 - v000) * stepY;
+                    final double d_dy__0y1 = (v011 - v001) * stepY;
+                    final double d_dy__1y0 = (v110 - v100) * stepY;
+                    final double d_dy__1y1 = (v111 - v101) * stepY;
+
+                    for (int yRel = 0; yRel < yScale; ++yRel) {
+                        double vxy0 = v0y0;
+                        double vxy1 = v0y1;
+                        final double d_dx__xy0 = (v1y0 - v0y0) * stepX;
+                        final double d_dx__xy1 = (v1y1 - v0y1) * stepX;
+
+                        // gradients start
+                        double v0yz = v0y0;
+                        double v1yz = v1y0;
+
+                        final double d_dz__0yz = (v0y1 - v0y0) * stepX;
+                        final double d_dz__1yz = (v1y1 - v1y0) * stepX;
+                        // gradients end
+
+                        for (int xRel = 0; xRel < xScale; ++xRel) {
+                            final double d_dz__xyz = (vxy1 - vxy0) * stepZ;
+                            double vxyz = vxy0;
+
+                            // gradients start
+                            final double d_dx__xyz = (v1yz - v0yz) * stepZ;
+                            gradX[yRel][xRel] = d_dx__xyz; // for this one x and z are swapped
+                            gradZ[xRel][yRel] = d_dz__xyz;
+                            // gradients end
+                            for (int zRel = 0; zRel < zScale; ++zRel) {
+                                // to get gradients working, consumer usage moved to later
+                                vals[xRel][yRel][zRel] = vxyz;
+                                vxyz += d_dz__xyz;
+                            }
+
+                            vxy0 += d_dx__xy0;
+                            vxy1 += d_dx__xy1;
+                            // gradients start
+                            v0yz += d_dz__0yz;
+                            v1yz += d_dz__1yz;
+                            // gradients end
+                        }
+
+                        v0y0 += d_dy__0y0;
+                        v0y1 += d_dy__0y1;
+                        v1y0 += d_dy__1y0;
+                        v1y1 += d_dy__1y1;
+
+                    }
+                    // gradients start
+                    double v00z = v000;
+                    double v01z = v010;
+                    double v10z = v100;
+                    double v11z = v110;
+
+                    final double d_dz__00z = (v001 - v000) * stepZ;
+                    final double d_dz__01z = (v011 - v010) * stepZ;
+                    final double d_dz__10z = (v101 - v100) * stepZ;
+                    final double d_dz__11z = (v111 - v110) * stepZ;
+
+                    for (int zRel = 0; zRel < zScale; ++zRel) {
+
+                        double vx0z = v00z;
+                        double vx1z = v01z;
+
+                        final double d_dx__x0z = (v10z - v00z) * stepX;
+                        final double d_dx__x1z = (v11z - v01z) * stepX;
+
+                        for (int xRel = 0; xRel < xScale; ++xRel) {
+
+                            double d_dy__xyz = (vx1z - vx0z) * stepY;
+
+                            gradY[xRel][zRel] = d_dy__xyz;
+
+                            vx0z += d_dx__x0z;
+                            vx1z += d_dx__x1z;
+                        }
+                        v00z += d_dz__00z;
+                        v01z += d_dz__01z;
+                        v10z += d_dz__10z;
+                        v11z += d_dz__11z;
+                    }
+
+                    for (int xRel = 0; xRel < xScale; ++xRel) {
+                        for (int zRel = 0; zRel < zScale; ++zRel) {
+                            for (int yRel = 0; yRel < yScale; ++yRel) {
+                                double vxyz = vals[xRel][yRel][zRel];
+                                double d_dx__xyz = gradX[yRel][zRel];
+                                double d_dy__xyz = gradY[xRel][zRel];
+                                double d_dz__xyz = gradZ[xRel][yRel];
+                                consumer.accept(x + xRel, y + yRel, z + zRel, d_dx__xyz, d_dy__xyz, d_dz__xyz, vxyz);
+                            }
+                        }
+                    }
+                    // gradients end
+                }
+            }
+        }
+    }
+
+    @Override
+    public void populate(Cube cube) {
+        if (PorkMethods.isCubeOutOfBounds(cube.getCoords())) {
+            return;
+        }
+
+        if (!MinecraftForge.EVENT_BUS.post(new CubePopulatorEvent(world, cube))) {
+            CubicBiome biome = CubicBiome.getCubic(cube.getCubicWorld().getBiome(Coords.getCubeCenter(cube)));
+
+            CubePos pos = cube.getCoords();
+            // For surface generators we should actually use special RNG with
+            // seed
+            // that depends only in world seed and cube X/Z
+            // but using this for surface generation doesn't cause any
+            // noticeable issues
+            Random rand = new Random(cube.cubeRandomSeed());
+
+            if (cube.getY() >= 1000 && cube.getY() <= 1015) {
+                endChunkGenerator.populate(cube.getX(), cube.getZ());
+
+                double j = rand.nextDouble();
+                if (j > 0.9) {
+                    BlockPos blockPos = new BlockPos(rand.nextInt(16) + cube.getCoords().getXCenter(), 16080, rand.nextInt(16) + cube.getCoords().getZCenter());
+                    for (; blockPos.y > 16030; blockPos.y--) {
+                        if (world.getBlockState(blockPos).getBlock() == Blocks.END_STONE) {
+                            break;
+                        }
+                    }
+
+                    if (blockPos.getY() > 16032) {
+                        blockPos.y += 1;
+                        BlockChorusFlower.generatePlant(World.class.cast(world), blockPos, rand, 8);
+                    }
+                }
+
+                j = rand.nextDouble();
+                if (j > 0.99) {
+                    BlockPos blockPos = new BlockPos(rand.nextInt(16) + cube.getCoords().getXCenter(), 16080, rand.nextInt(16) + cube.getCoords().getZCenter());
+                    for (; blockPos.y > 16030; blockPos.y--) {
+                        if (world.getBlockState(blockPos).getBlock() == Blocks.END_STONE) {
+                            break;
+                        }
+                    }
+
+                    if (blockPos.getY() > 16032) {
+                        blockPos.y += 1;
+                        EntityShulker shulker = new EntityShulker(World.class.cast(world));
+                        world.spawnEntity(shulker);
+                        shulker.setPosition(blockPos.x + .5f, blockPos.y, blockPos.z + .5f);
+                    }
+                }
+
+                return;
+            }
+
+            ICubicPopulator decorator = biome.getDecorator();
+            decorator.generate(world, rand, pos, biome);
+            CubeGeneratorsRegistry.generateWorld(world, rand, pos, biome);
+
+            strongholds.generateStructure((World) world, rand, pos);
+        }
     }
 }
